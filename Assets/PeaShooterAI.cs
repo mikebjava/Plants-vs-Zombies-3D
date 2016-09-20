60a69d2dd82629b99a9baa86b373976e64a8302a
michaelbehner96 <michaelbehner96@gmail.com>
2016-09-20 16:22:59 -0400

    First

---
+++ Assets/PeaShooterAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PeaShooterAI : MonoBehaviour
+{
+    [Tooltip("The Game Object that holds the transform value for the starting position of the attack projectiles.")]
+    public GameObject FireLocation;
+    public GameObject Projectile;
+    private Animator animator;
+
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+        if (animator == null)
+            this.animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Shoot");
+
+        if (Projectile != null && FireLocation != null)
+        {
+            Instantiate(Projectile, FireLocation.transform.position, Projectile.transform.rotation);
+        }
+    }
+}
---
+++ Assets/ProjectileAI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileAI : MonoBehaviour
+{
+    [Tooltip("The life time of the projectile, in seconds.")]
+    public int LifeTime = 5;
+    public float Speed = 0.5f;
+    [Tooltip("Direction that the projectile travels. (Default Left -> Right)")]
+    public Vector3 Direction = new Vector3(0f, 0f, 1f);
+
+    private float CreationTime;
+
+    void Start()
+    {
+        CreationTime = Time.time;
+    }
+
+    void FixedUpdate()
+    {
+        if (CreationTime + LifeTime <= Time.time)
+        {
+            GameObject.Destroy(this.gameObject);
+            return;
+        }
+
+        transform.position += Direction * Speed;
+    }
+}
